7ec7b4c1c44cb0730b4b99410745f522edab5875
Nathan Edwards <nathan@onedarnleyroad.com>
2017-11-03 12:17:21 +0000

    allow units

--- index.js
+++ index.js
@@ -26,20 +26,22 @@ module.exports = (function(w) {
     */
 
     var defaults = {
-        desktop: { min: 1367 },
-        laptop: { max: 1366 },
-        tablet: { max: 1024 },
-        mobile: { max: 480 },
+        desktop: { min: 1367, unit: 'px' },
+        laptop: { max: 1366, unit: 'px' },
+        tablet: { max: 1024, unit: 'px' },
+        mobile: { max: 480, unit: 'px' },
     };
 
     // can we use the matchMedia api?
     // if not use plain old jQuery window size.
     var mq = ('matchMedia' in window);
 
-    var _maxWidth = function( size ) {
+    var _maxWidth = function( size, unit ) {
+
+        unit = unit || 'px';
 
         if (mq) {
-            return ( window.matchMedia('(max-width: ' + size + 'px)').matches );
+            return ( window.matchMedia('(max-width: ' + size + unit + ')').matches );
         } else {
             return ( $(window).width() <= size );
         }
@@ -47,7 +49,7 @@ module.exports = (function(w) {
 
     var _minWidth = function( size ) {
         if (mq) {
-            return ( window.matchMedia('(min-width: ' + size + 'px)').matches );
+            return ( window.matchMedia('(min-width: ' + size + unit + ')').matches );
         } else {
             return ( $(window).width() > size );
         }
@@ -71,7 +73,13 @@ module.exports = (function(w) {
 
         if (!rule) { return -1; }
 
+        var unit = 'px';
+        if (rule.hasOwnProperty('unit')) {
+            unit = rule.unit;
+        }
+
         if (rule.hasOwnProperty( 'max' )) {
+
 
             if (!_maxWidth( rule.max )) {
                 return false;
